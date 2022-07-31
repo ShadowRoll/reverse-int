ab1b3f2baf4e5e31fbadfaaec61b1cb9039dcd7e
Ilya <70850776+ShadowRoll@users.noreply.github.com>
2022-07-31 19:03:06 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let modul = Math.abs(n);
+    let num = Array.from(modul.toString(), String); 
+    let reversed = num.reverse();
+    let result = Number(reversed[0] + reversed[1] + reversed[2]);
+
+    return result;
 }
